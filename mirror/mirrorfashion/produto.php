0792ad9287881c43a9d25cd495fef0b4a8d8a4db
Jamil Martinez <jamil.cmc@gmail.com>
2017-09-18 09:04:03 -0300

    - product page: Added color selection button, size selection and clothing information in a table

--- mirror/mirrorfashion/produto.php
+++ mirror/mirrorfashion/produto.php
@@ -1,41 +1,86 @@
-
-    <!--Cabeçalho -->
-    <?php
+<!--Cabeçalho -->
+<?php
         $cabecalho_title = "Produto da Mirror Fashion";
         $cabecalho_css = '  <link rel="stylesheet" href="css/reset.css">
                             <link rel="stylesheet" href="css/estilos.css">
                             <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
+                            <link rel="stylesheet" href="css/produto.css">
                          ';
         include("cabecalho.php");
     ?>
-        <div class="produto">
-            <h1> Fuzzy Cardigan </h1>
-            <p> por apenas R$ 129,90 </p>
+    <div class="produto-back">
+        <div class="container">
+            <div class="produto">
+                <h1> Fuzzy Cardigan </h1>
+                <p> por apenas R$ 129,90 </p>
 
-            <form>
-                <fieldset class="cores">
-                    <legend>Escolha a cor </legend>
+                <form>
+                    <fieldset class="cores">
+                        <legend>Escolha a cor </legend>
 
-                    <input type="radio" name="cor" value="verde" id="verde" checked>
-                    <label for="verde">
+                        <input type="radio" name="cor" value="verde" id="verde" checked>
+                        <label for="verde">
                             <img src="img/produtos/foto2-verde.png" alt="verde">
                     </label>
 
-                    <input type="radio" name="cor" value="rosa" id="rosa">
-                    <label for="rosa">
+                        <input type="radio" name="cor" value="rosa" id="rosa">
+                        <label for="rosa">
                         <img src="img/produtos/foto2-rosa.png" alt="rosa">
                     </label>
 
-                    <input type="radio" name="cor" value="azul" id="azul">
-                    <label for="azul">
+                        <input type="radio" name="cor" value="azul" id="azul">
+                        <label for="azul">
                         <img src="img/produtos/foto2-azul.png" alt="azul">
                     </label>
-                </fieldset>
+                    </fieldset>
 
-                <input type="submit" class="comprar" value="Comprar">
+                    <fieldset class="tamanhos">
+                        <legend> Escolha o tamanho: </legend>
 
-            </form>
+                        <input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho">
+                    </fieldset>
 
+                    <input type="submit" class="comprar" value="Comprar">
+
+                </form>
+
+            </div>
+
+            <div class="deatalhe">
+                <h2> Detalhes do produto</h2>
+
+                <p>Esse é o melhor casaco de Cardigã que você já viu. Excelente material italiano com estampa desenhada pelos
+                    artesãos da comunidade de Krotor nas ilhas gregas. Compre já e receba hoje mesmo pela nossa entrega ajato.</p>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th> Características </th>
+                            <th> Detalhe </th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td> Modelo </td>
+                            <td> Cardigã </td>
+                        </tr>
+                        <tr>
+                            <td>Material </td>
+                            <td>Algodão e poliester </td>
+                        </tr>
+                        <tr>
+                            <td>Cores </td>
+                            <td>Verde, Rosa e Azul</td>
+                        </tr>
+                        <tr>
+                            <td> Lavagem </td>
+                            <td> Lavar a mão </td>
+                        </tr>
+                    </tbody>
+
+                </table>
+            </div>
         </div>
-
+    </div>
     <?php include("rodape.php") ?>
